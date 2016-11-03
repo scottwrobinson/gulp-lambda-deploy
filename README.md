8ff027f604f066d19d9379380247b9bde3291957
Scott Robinson <s.w.robinson@gmail.com>
2016-11-03 16:13:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,103 @@
+# gulp-lambda-deploy
+
+> Gulp plugin to deploy zipped packages to AWS Lambda directly or via S3
+
+## Install
+
+```bash
+$ npm install --save-dev gulp-lambda-deploy
+```
+
+## Usage
+
+`gulp-lambda-deploy` accepts a single zip file, which is uploaded to AWS Lambda either directly or via S3. After upload, the zip file is passed down the stream. I suggest using [gulp-zip](https://github.com/sindresorhus/gulp-zip) to create the zip file.
+
+Here is the typical usage:
+
+```js
+const gulp = require('gulp');
+const lambda = require('gulp-lambda-deploy');
+const zip = require('gulp-zip');
+
+let params = {
+    name: '...',
+    role: '...',
+    s3: {
+        bucket: '...',
+        key: '...'
+    }
+};
+
+let options = {
+    profile: '...',
+    region: '...'
+};
+
+gulp.task('default', function(cb) {
+    return gulp.src('index.js')
+        .pipe(zip('package.zip'))
+        .pipe(lambda(params, options))
+        .on('end', cb);
+});
+```
+
+If the `params.s3` object is not provided, then the zip file is uploaded directly to Lambda and is not stored in S3 as an intermediate step.
+
+If a function with the name given in `params.name` does not already exist, then it is created, otherwise the existing code and configuration is updated for that function.
+
+For more information on `params` and `options` see the [API section](#api).
+
+#### AWS Credentials
+
+Currently you must provide an AWS profile name that is stored in the Shared Credentials File at `~/.aws/credentials`. For more info see the [aws-sdk documentation](http://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-shared.html).
+
+## API
+
+`gulp-lambda-deploy` must be given exactly two arguments. The first is Lambda-specific parameters, and the second is options for AWS. Each has its own set of optional and required properties.
+
+```js
+lambda(params, options);
+```
+
+### `params`
+
+These parameters are specific to the Lambda function you're uploading.
+
+#### `name` (required)
+The name of your function.
+
+#### `role` (required)
+The ARN of the role given to the Lambda function.
+
+#### `s3`
+An object indicating where to upload the function code to. If the `s3` parameter is given, it must contain the properties `bucket` and `key`. These specify the upload location for the file within S3.
+
+#### `handler`
+The location of your method handler. Must be of the form `[filename].[method name]` where `[method name]` is exported. Defaults to `index.handler`.
+
+#### `runtime`
+The runtime for your code. Defaults to `nodejs4.3`.
+
+#### `publish`
+A boolean indidcating if you want to publish a new version of the function.
+
+#### `description`
+A string describing the Lambda function.
+
+#### `timeout`
+The time in seconds that the function is allowed to run on each invocation.
+
+### `options`
+
+Options for configuring the AWS environment.
+
+#### `profile` (required)
+The named AWS profile used to authenticate and interact with AWS services. Must be saved in Shared Credentials File. See the [AWS Credentials](#aws-credentials) section.
+
+#### `region` (required)
+The AWS region your resources will be located in.
+
+<br/>
+> [stackabuse.com](http://stackabuse.com) &nbsp;&middot;&nbsp;
+> Twitter [@ScottWRobinson](https://twitter.com/scottwrobinson) &nbsp;&middot;&nbsp;
+> GitHub [@ScottWRobinson](https://github.com/scottwrobinson)
---
+++ index.js
@@ -0,0 +1,198 @@
+'use strict';
+
+const assert = require('assert');
+const AWS = require('aws-sdk');
+const gutil = require('gulp-util');
+const through = require('through2');
+
+const DEFAULT_PARAMS = {
+    handler: 'index.handler',
+    runtime: 'nodejs4.3'
+};
+
+let gulpError = function(message) {
+    return new gutil.PluginError('gulp-lambda-deploy', message);
+};
+
+module.exports = function(params, options) {
+    if (!params) throw gulpError('No parameters provided');
+    if (!params.name) throw gulpError('No Lambda function name provided');
+    if (!params.role) throw gulpError('No Lambda role provided');
+    if (!options) throw gulpError('No AWS options provided');
+    if (!options.region) throw gulpError('No AWS region provided');
+    if (!options.profile) throw gulpError('No AWS profile provided');
+
+    if (params.s3) {
+        if (!params.s3.bucket) throw gulpError('If uploading via S3, a bucket must be provided');
+        if (!params.s3.key) throw gulpError('If uploading via S3, a key must be provided');
+    }
+
+    AWS.config.credentials = new AWS.SharedIniFileCredentials({
+        profile: options.profile
+    });
+
+    let s3 = new AWS.S3({
+        region: options.region
+    });
+
+    let lambda = new AWS.Lambda({
+        region: options.region
+    });
+
+    let transform = function(file, enc, cb) {
+        if (file.isNull()) {
+            return cb();
+        }
+
+        if (file.isStream()) {
+            throw gulpError('Stream content is not supported');
+        }
+
+        params.file = file;
+        cb();
+    };
+
+    let flush = function(cb) {
+        gutil.log('Uploading Lambda function "' + params.name + '"...');
+
+        let stream = this;
+        let done = function(err) {
+            if (err) return cb(gulpError(err.message));
+            gutil.log('Lambda function "' + params.name + '" successfully uploaded');
+            stream.push(params.file);
+            cb();
+        };
+
+        if (!params.file) {
+            return cb(gulpError('No code provided'));
+        }
+
+        if (params.file.path.slice(-4) !== '.zip') {
+            return cb(gulpError('Given file is not a zip'));
+        }
+
+        Promise.resolve().then(function() {
+            if (params.s3) {
+                // Upload Lambda code via S3
+                return s3upload(s3, params);
+            }
+        }).then(function() {
+            // Check if function already exists...
+            return hasLambdaFunction(lambda, params.name);
+        }).then(function(hasFunction) {
+            if (hasFunction) {
+                // ...if it does, then update code/config...
+                return updateFunctionCode(lambda, params).then(function() {
+                    return updateFunctionConfiguration(lambda, params);
+                });
+            }
+            // ...if it doesn't, then create it
+            return createFunction(lambda, params);
+        }).then(function() {
+            done();
+        }).catch(function(err) {
+            done(err);
+        });
+    };
+
+    return through.obj(transform, flush);
+};
+
+function s3upload(s3, params) {
+    return new Promise(function(resolve, reject) {
+        var s3params = {
+            Bucket: params.s3.bucket,
+            Key: params.s3.key,
+            Body: params.file.contents
+        };
+
+        s3.putObject(s3params, function(err, data) {
+            if (err) reject(err);
+            resolve(data);
+        });
+    });
+}
+
+function hasLambdaFunction(lambda, targetFunction) {
+    return new Promise(function(resolve, reject) {
+        lambda.listFunctions({}, function(err, data) {
+            if (err) return reject(err);
+            resolve(data.Functions);
+        });
+    }).then(function(functions) {
+        for (let i = 0; i < functions.length; i++) {
+            if (functions[i].FunctionName === targetFunction) {
+                return true;
+            }
+        }
+        return false;
+    });
+}
+
+function updateFunctionCode(lambda, params) {
+    var lamparams = {
+        Publish: params.publish,
+        FunctionName: params.name
+    };
+
+    if (params.s3) {
+        lamparams.S3Bucket = params.s3.bucket;
+        lamparams.S3Key = params.s3.key;
+    } else {
+        lamparams.ZipFile = params.file.contents;
+    }
+
+    return new Promise(function(resolve, reject) {
+        lambda.updateFunctionCode(lamparams, function(err, data) {
+            if (err) return reject(err);
+            resolve(data);
+        });
+    });
+}
+
+function updateFunctionConfiguration(lambda, params) {
+    var lamparams = {
+        FunctionName: params.name,
+        Role: params.role,
+        Handler: params.handler || DEFAULT_PARAMS.handler,
+        Runtime: params.runtime || DEFAULT_PARAMS.runtime,
+    };
+
+    if (params.description) lamparams.Description = params.description;
+    if (params.timeout) lamparams.Timeout = params.timeout;
+
+    return new Promise(function(resolve, reject) {
+        lambda.updateFunctionConfiguration(lamparams, function(err, data) {
+            if (err) return reject(err);
+            resolve(data);
+        });
+    });
+}
+
+function createFunction(lambda, params) {
+    let code = {};
+    if (params.s3) {
+        code.S3Bucket = params.s3.bucket;
+        code.S3Key = params.s3.key;
+    } else {
+        code.ZipFile = params.file.contents;
+    }
+
+    var lamparams = {
+        Code: code,
+        FunctionName: params.name,
+        Handler: params.handler || DEFAULT_PARAMS.handler,
+        Runtime: params.runtime || DEFAULT_PARAMS.runtime,
+        Role: params.role
+    };
+
+    if (params.description) lamparams.Description = params.description;
+    if (params.timeout) lamparams.Timeout = params.timeout;
+
+    return new Promise(function(resolve, reject) {
+        lambda.createFunction(lamparams, function(err, data) {
+            if (err) return reject(err);
+            resolve(data);
+        });
+    });
+}
